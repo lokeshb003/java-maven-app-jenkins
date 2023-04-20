37bf9499193bedac12fe809dd23339e110ce6390
Lokesh B <lokesh.b.2020.ad@ritchennai.edu.in>
2023-04-20 10:06:19 +0530

    Errors fixed

--- src/test/java/com/mycompany/app/AppTest.java
+++ src/test/java/com/mycompany/app/AppTest.java
@@ -20,6 +20,6 @@ public class AppTest
     public void testAppMessage()
     {
         App app = new App();
-        assertEquals("Hello World!", app.getMessage());
+        assertEquals("Hello I am Lokesh Balaji! Nice to Meet you!", app.getMessage());
     }
 }
